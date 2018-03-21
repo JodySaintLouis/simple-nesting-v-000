1e2d6beb353cf890a0039761429b5dc3b4098094
Jamal Gelin <gelin.jamal@gmail.com>
2018-03-21 12:28:35 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -92,11 +92,11 @@ def changing_alan
         },
         :alan_kay => {
           :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
+          :languages => {"Smalltalk", "LISP"}
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => {"C"}
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
