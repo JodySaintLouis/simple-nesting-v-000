734469032a1736194ff79a92dcc296568dda076a
Jamal Gelin <gelin.jamal@gmail.com>
2018-03-21 12:28:21 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -110,15 +110,15 @@ def adding_to_dennis
  		{
         :grace_hopper => {
           :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
+          :languages => {"COBOL", "FORTRAN"}
         },
         :alan_kay => {
           :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
+          :languages => {"Smalltalk", "LISP"}
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => {"C"}
         }
      }
 
